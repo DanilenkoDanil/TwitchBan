46094fa36aef70fd3c1edfa079ac44475ea7c46e
Данил <danilenkodanil74@gmail.com>
2023-03-10 21:51:41 +0200

    country

--- twitch_ban/base/models.py
+++ twitch_ban/base/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 
 class Chanel(models.Model):
-    county_choices = (
+    county_choices = [
         ('gr', 'Gr'),
         ('fi', 'Fi'),
         ('no', 'No'),
         ('ca', 'Ca'),
-    )
+    ]
     title = models.CharField(max_length=256)
     link = models.CharField(max_length=256)
     ban = models.BooleanField(default=False)
     check_date = models.DateTimeField(auto_now=True)
     add_date = models.DateTimeField(auto_now=True)
     ban_date = models.DateTimeField(blank=True, null=True)
-    county = models.Choices(county_choices)
+    county = models.IntegerField(choices=county_choices)
     spectators = models.IntegerField(default=0)
     subscribers = models.IntegerField(default=0)
     notes = models.TextField(default='')
