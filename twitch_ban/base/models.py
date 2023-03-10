99e5268624c66d4d6f42041c605e48fa87ff8796
Данил <danilenkodanil74@gmail.com>
2023-03-10 21:55:08 +0200

    country

--- twitch_ban/base/models.py
+++ twitch_ban/base/models.py
@@ -14,7 +14,7 @@ class Chanel(models.Model):
     check_date = models.DateTimeField(auto_now=True)
     add_date = models.DateTimeField(auto_now=True)
     ban_date = models.DateTimeField(blank=True, null=True)
-    county = models.IntegerField(choices=county_choices)
+    county = models.CharField(choices=county_choices, max_length=100)
     spectators = models.IntegerField(default=0)
     subscribers = models.IntegerField(default=0)
     notes = models.TextField(default='')
